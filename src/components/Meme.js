a658d49598a58873ad7e849346675506362e9208
fadega <fadega.araya@gmail.com>
2023-03-18 23:57:39 -0600

    README.md updated

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,34 +1,72 @@
 import React from "react";
 import memesData from "../memesData.js";
 export default function Meme() {
-    const [memeImage, setMemeIamge] = React.useState("");
+    //method 1 - fetching data from the api
+    // const [memeImage, setMemeIamge] = React.useState("https://i.imgflip.com/30b1gx.jpg");
+    
+    //method 2 - fetching data from the local file 
+      //passing a default image to the state - u can pass empty string as well
+     // const [memeImage, setMemeIamge] = React.useState("https://i.imgflip.com/30b1gx.jpg");
+  //  const [memeImage, setMemeIamge] = React.useState("https://i.imgflip.com/30b1gx.jpg");
+  //  let topText=""
+  //  let bottomText = ""
+  //  function getMemeImage() {
+  //       const memes = memesData.map((meme) => {
+  //           const memesArray = meme.data.memes;
+  //           const randomIndex = Math.floor(Math.random() * memesArray.length);
+
+  //           topText = document.querySelector(".one").value;
+  //           bottomText = document.querySelector(".two").value;
+  //           console.log(` Top text: ${topText} Bottom text: ${bottomText}`);
+  //           setMemeIamge(memesArray[randomIndex].url);    
+            
+  //       })
+  //   }
+
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomIamge: "https://i.imgflip.com/30b1gx.jpg"
+      })
+    
+    const [allMemes, setAllMemes] = React.useState(memesData);
+
     function getMemeImage() {
-        const memes = memesData.map((meme) => {
+        const memes = allMemes.map((meme) => {
             const memesArray = meme.data.memes;
             const randomIndex = Math.floor(Math.random() * memesArray.length);
-            setMemeIamge(memesArray[randomIndex].url);
-           
+            setMeme({
+                topText: document.querySelector(".one").value,
+                bottomText: document.querySelector(".two").value,
+                randomIamge: memesArray[randomIndex].url
+            })
         })
-
     }
-  
     return (
       <main className="meme">
           <div className="form">
-              <input 
-                  type="text" 
-                  className="form--input"
-                  placeholder="Top text"
-              />
-              <input 
-                  type="text" 
-                  className="form--input"
-                  placeholder="Bottom text"
-              />
+            <div className="form--inputs">
+                <input 
+                    type="text" 
+                    className="form--input one"
+                    placeholder="Top text"
+                />
+                <input 
+                    type="text" 
+                    className="form--input two"
+                    placeholder="Bottom text"
+                />
+            </div>
+              
               <button className="form--button" onClick={getMemeImage}>
                   Get new meme image
               </button>
-              <img src={memeImage} alt="meme" className="meme--image"/>
+              <img src={meme.randomIamge} alt="meme" className="meme--image"/>
+             <div className="meme--text">
+                <p className="top--text">{meme.topText}</p>
+                <p className="bottom--text">{meme.bottomText}</p>
+            </div> 
+           
           </div>
        
       </main>
